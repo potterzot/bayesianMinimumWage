96b07598e8934981c93852f5f90cfe829c81f441
Nicholas Potter <potterzot@gmail.com>
2017-04-12 19:08:30 -0700

    fresh analysis with QCEW data

--- code/impact_example.R
+++ code/impact_example.R
@@ -1,11 +1,14 @@
 # Example estimation of impact
 library(data.table)
+library(zoo)
 library(bsts)
 library(CausalImpact)
 
-#read in the QCEW data
-#df <- fread("data/out/qcew/emp.csv", stringsAsFactors = FALSE)
+#read in the QCEW data in long format
+dt <- fread("data/out/qcew/emp.csv", stringsAsFactors = FALSE)
 
+#convert to wide format
+dt_final <- dcast(dt, year + qtr ~ area_fips + industry_code, value.var = (names(dt)[5:8]))
 
 #dates - needed for time series
 pre_period <- as.Date(c("1990-01-01", "2006-06-01"))
@@ -15,23 +18,26 @@ nrows <- length(dates)
 pre_periods <- length(dates[dates >= pre_period[1] & dates <= pre_period[2]])
 post_periods <- length(dates[dates >= post_period[1] & dates <= post_period[2]])
 
-#synthetic data
-df <- data.frame(
-  date = dates,
-  sf_emp_private = cumsum(rnorm(nrows, mean = 20, sd = 100)) + 1000,
-  sf_hours_private = cumsum(rnorm(nrows, mean = 35, sd = 10)),
-  sf_hourly_earnings_private = cumsum(rnorm(nrows, mean = 10, sd = 5)),
-  abq_emp_private = cumsum(rnorm(nrows, mean = 20, sd = 100)) + 1000,
-  abq_hours_private = cumsum(rnorm(nrows, mean = 35, sd = 10)),
-  abq_hourly_earnings_private = cumsum(rnorm(nrows, mean = 10, sd = 5))
-)
-sf_emp_retail <- df$sf_emp_private * (0.1 + (rnorm(1, 0, 0.1))^2) * c(rep(1,pre_periods), rep(0.9, post_periods))
+#synthetic data - alternative for testing
+# df <- data.frame(
+#   date = dates,
+#   sf_emp_private = cumsum(rnorm(nrows, mean = 20, sd = 100)) + 1000,
+#   sf_hours_private = cumsum(rnorm(nrows, mean = 35, sd = 10)),
+#   sf_hourly_earnings_private = cumsum(rnorm(nrows, mean = 10, sd = 5)),
+#   abq_emp_private = cumsum(rnorm(nrows, mean = 20, sd = 100)) + 1000,
+#   abq_hours_private = cumsum(rnorm(nrows, mean = 35, sd = 10)),
+#   abq_hourly_earnings_private = cumsum(rnorm(nrows, mean = 10, sd = 5))
+# )
+
+# Seperate the dependent and independent variables
+X <- dt_final[, -c("emp_35049_722", "year", "qtr"), with=F]
+Y <- dt_final[, emp_35049_722]
 
 # Time series data
-data <- zoo(cbind(sf_emp_retail, df[, 2:ncol(df)]), dates)
+data <- zoo(cbind(Y, X), dates)
 
 #now determine the impact 
-impact <- CausalImpact(data, pre_period, post_period, model.args = list(nseasons = 4, season.duration = 3))
+impact <- CausalImpact(data, pre_period, post_period, model.args = list(nseasons = 4, season.duration = 1))
 plot(impact)
 summary(impact)
 
